8bdc648ffa886a0e5b7f61808162ecb6ca2e3dfa
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-10-13 03:14:59 +0600

    module 10 assignment Updated.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color tdBgColor = Color(0xFFEEEFF5);
-const Color tdBgBlack = Color(0xFF3B3B3F);
-const Color tdColorBlack = Color(0xFF2F2E2E);
-const Color tdButtonColor = Color(0xFFEEEFF5);
-const Color colorWhite = Color(0xFFFFFFFF);
+const Color bgColorWhite = Color(0xFFEEEFF5);
+const Color bgColorBlack = Color(0xFF3B3B3F);
+const Color colorGreen = Color(0xFF0EEC9E);
+const Color colorDeepOrange = Color(0xFFFF5722);
 
--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -14,14 +14,112 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         elevation: 4,
-        backgroundColor: tdBgColor,
+        backgroundColor: bgColorWhite,
         actions: [
           IconButton(
             onPressed: () {},
             icon: Icon(Icons.search),
-            color: tdBgBlack,
+            color: bgColorBlack,
           )
         ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 5),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: "Add Title",
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                  borderSide: BorderSide(color: colorGreen),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 5),
+                            child: TextFormField(
+                              maxLines: 2,
+                              decoration: const InputDecoration(
+                                hintText: "Add Description",
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                  borderSide: BorderSide(color: colorGreen),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 35,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: colorDeepOrange,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  )),
+                              child: Text("Add"),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Expanded(
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: ListView.builder(
+                            itemCount: 10,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading: Icon(Icons.star),
+                                title: Text('ListTile 1'),
+                                subtitle: Text('Subtitle 1'),
+                                onTap: () {
+                                  // Handle onTap for ListTile 1
+                                },
+                              );
+                            }),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
